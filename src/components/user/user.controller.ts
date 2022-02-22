910ef9430a52dcb78799036718805f8ab832aa8d
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-02-22 21:45:39 +0200

    Hashed Password With salt and pepper Using bcrypt

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,38 +1,38 @@
 import { Request, Response } from 'express';
 import { ICreateUser } from './user.interfaces';
 import User from './user.model';
+import bcrypt from 'bcryptjs';
+require('dotenv').config();
 
 class UserController {
-
-  async getUser(req: Request, res: Response){
+  async getUser(req: Request, res: Response) {
     const user = await User.findOneById(+req.params.id);
-    if(!user){
+    if (!user) {
       res.status(404).send({ message: 'User Not Found!' });
     }
     res.send(user);
   }
 
-  async getUsers(req: Request, res: Response){
+  async getUsers(req: Request, res: Response) {
     const users = await User.findAll();
     res.send(users);
   }
 
-  async signUp(req: Request, res: Response){
-    const {
-      firstname,
-      lastname,
-      email,
-      password,
-    } = req.body;
-    console.log(req.body);
+  async signUp(req: Request, res: Response) {
 
+    const { firstname, lastname, email, password } = req.body;
+    const pepper = process.env.BCRYPT_PASSWORD;
+    const saltRounds = process.env.SALT_ROUNDS;
+    const hashPassword = bcrypt.hashSync(password + pepper, parseInt(saltRounds as string, 10));
     const existingUser = await User.findOneByEmail(email);
 
-    if(existingUser){
-      return res.status(400).send({ message: 'There\'s a user with this email already!' });
+    if (existingUser) {
+      return res
+        .status(400)
+        .send({ message: 'There\'s a user with this email already!' });
     }
 
-    const dataObject: ICreateUser = { firstname, lastname, email, password };
+    const dataObject: ICreateUser = { firstname, lastname, email, password:hashPassword };
 
     const user = await User.create(dataObject);
     res.status(201).send(user);
