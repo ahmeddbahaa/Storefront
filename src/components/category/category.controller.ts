7994546d95ae22b2bd1aeccf28fc1f6b82dad29b
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-02-26 04:19:18 +0200

    Added Try-catch blocks for asynchronous methods in Category component

--- src/components/category/category.controller.ts
+++ src/components/category/category.controller.ts
@@ -1,17 +1,26 @@
 import { Request, Response} from 'express';
+import Logger from '../../middlewares/logger';
 import { ICreateCategory } from './category.interfaces';
 import Category from './category.model';
 class CategoryController{
   async getCategories(req: Request, res: Response){
-    const rows = await Category.findAll();
-    res.send(rows);
+    try {
+      const rows = await Category.findAll();
+      res.send(rows);
+    } catch (error) {
+      Logger.log('Category Controller Error: ',error);
+    }
   }
 
   async create(req: Request, res: Response){
-    const{name} = req.body;
-    const dataObject:ICreateCategory = {name};
-    const category = await Category.create(dataObject);
-    res.status(201).send(category);
+    try {
+      const{name} = req.body;
+      const dataObject:ICreateCategory = {name};
+      const category = await Category.create(dataObject);
+      res.status(201).send(category);
+    } catch (error) {
+      Logger.log('Category Controller Error: ',error);
+    }
   }
 }
 export default new CategoryController();
--- src/components/category/category.model.ts
+++ src/components/category/category.model.ts
@@ -1,24 +1,32 @@
+import Logger from '../../middlewares/logger';
 import Common from '../../utils/common';
 import { ICategory, ICreateCategory, ICategorySeralized } from './category.interfaces';
 class Category {
   static tableName: string = 'categories';
   static async create(category: ICreateCategory): Promise<ICategory | null> {
-    const insertedQuery = await Common.dbInsertion(Category.tableName, category);
-    if (insertedQuery && insertedQuery.inserted) {
-      const category = insertedQuery.data[0] as ICategory;
-      return category;
-    }else {
-      return null;
+    try {
+      const insertedQuery = await Common.dbInsertion(Category.tableName, category);
+      if (insertedQuery && insertedQuery.inserted) {
+        const category = insertedQuery.data[0] as ICategory;
+        return category;
+      }
+    } catch (error) {
+      Logger.log('Category Model Error: ',error);
     }
+    return null;
   }
   static async findAll(): Promise<ICategorySeralized[]|null> {
-    const rows = await Common.dbFetch(Category.tableName);
-    if(rows?.length){
-      const categories = rows as ICategorySeralized[];
-      return categories;
-    }else{
-      return null;
+    try {
+      const rows = await Common.dbFetch(Category.tableName);
+      if(rows?.length){
+        const categories = rows as ICategorySeralized[];
+        return categories;
+      }
+    } catch (error) {
+      Logger.log('Category Model Error: ',error);
     }
+    return null;
+
   }
 }
 export default Category;
