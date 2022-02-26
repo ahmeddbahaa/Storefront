0fbb5108b177c7e23e6d6cd85e406dda525d353c
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-02-26 03:37:46 +0200

    Added Try-catch blocks for all asynchronous methods

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -3,60 +3,76 @@ import { ICreateUser } from './user.interfaces';
 import User from './user.model';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import Logger from '../../middlewares/logger';
 require('dotenv').config();
 
 class UserController {
   async getUser(req: Request, res: Response) {
-    const user = await User.findOneById(+req.params.id);
-    if (!user) {
-      res.status(404).send({ message: 'User Not Found!' });
+    try {
+      const user = await User.findOneById(+req.params.id);
+      if (!user) {
+        res.status(404).send({ message: 'User Not Found!' });
+      }
+      res.send(user);
+    } catch (error) {
+      Logger.log('User Controller Error', error);
     }
-    res.send(user);
   }
 
   async getUsers(req: Request, res: Response) {
-    const users = await User.findAll();
-    res.send(users);
+    try {
+      const users = await User.findAll();
+      res.send(users);
+    } catch (error) {
+      Logger.log('User Controller Error', error);
+    }
   }
 
   async signUp(req: Request, res: Response) {
-    const { firstname, lastname, email, password } = req.body;
-    const pepper = process.env.BCRYPT_PASSWORD;
-    const saltRounds = process.env.SALT_ROUNDS as string;
-    const hashPassword = bcrypt.hashSync(
-      password + pepper,
-      parseInt(saltRounds));
-    const existingUser = await User.findOneByEmail(email);
+    try {
+      const { firstname, lastname, email, password } = req.body;
+      const pepper = process.env.BCRYPT_PASSWORD;
+      const saltRounds = process.env.SALT_ROUNDS as string;
+      const hashPassword = bcrypt.hashSync(
+        password + pepper,
+        parseInt(saltRounds));
+      const existingUser = await User.findOneByEmail(email);
 
-    if (existingUser) {
-      return res
-        .status(400)
-        .send({ message: 'Theres a user with this email already!' });
+      if (existingUser) {
+        return res
+          .status(400)
+          .send({ message: 'Theres a user with this email already!' });
+      }
+      const dataObject: ICreateUser = {
+        firstname,
+        lastname,
+        email,
+        password: hashPassword,
+      };
+      const user = await User.create(dataObject);
+      res.status(201).send(user);
+    } catch (error) {
+      Logger.log('User Controller Error', error);
     }
-
-    const dataObject: ICreateUser = {
-      firstname,
-      lastname,
-      email,
-      password: hashPassword,
-    };
-
-    const user = await User.create(dataObject);
-    res.status(201).send(user);
   }
   async signIn(req: Request, res: Response) {
-    const {email,  password} = req.body;
-    const user = await User.findOneByEmail((email as unknown) as string);
-    if(user == null){
-      res.status(401).send({ message: 'Error No User Found with given Email !'});
-      return;
+    try {
+      const {email,  password} = req.body;
+      const user = await User.findOneByEmail((email as unknown) as string);
+      if(user == null){
+        res.status(401).send({ message: 'Error No User Found with given Email !'});
+        return;
+      }
+      if(!bcrypt.compare((password as unknown) as string, user.password)){
+        res.status(401).send({ message: 'Error Wrong Password for given User !'});
+        return;
+      }
+      const token = jwt.sign({user: user}, process.env.JWT_KEY as jwt.Secret);
+      res.header({Authorization: 'Bearer ' + token}).send({status:'ok',token:token});
+    } catch (error) {
+      Logger.log('User Controller Error', error);
     }
-    if(!bcrypt.compare((password as unknown) as string, user.password)){
-      res.status(401).send({ message: 'Error Wrong Password for given User !'});
-      return;
-    }
-    const token = jwt.sign({user: user}, process.env.JWT_KEY as jwt.Secret);
-    res.header({Authorization: 'Bearer ' + token}).send({status:'ok',token:token});
+
   }
 }
 
--- src/components/user/user.model.ts
+++ src/components/user/user.model.ts
@@ -1,3 +1,4 @@
+import Logger from '../../middlewares/logger';
 import Common from '../../utils/common';
 import { ICreateUser, IUser, IUserSerialized } from './user.interfaces';
 
@@ -5,16 +6,19 @@ class User {
   static tableName: string = 'users';
 
   static async findOneById(id: number): Promise<IUserSerialized | null>{
-    const rows = await Common.dbFetch(User.tableName, { id });
-    if(rows?.length){
-      const user = rows[0] as IUserSerialized;
+    try {
+      const rows = await Common.dbFetch(User.tableName, { id });
+      if(rows?.length){
+        const user = rows[0] as IUserSerialized;
 
-      // removing password from the object before serializing it
-      user.password = undefined;
-      return user;
-    }else{
-      return null;
+        // removing password from the object before serializing it
+        user.password = undefined;
+        return user;
+      }
+    } catch (error) {
+      Logger.log('User Model Error', error);
     }
+    return null;
   }
 
   static async findOneByEmail(email: string): Promise<IUser | null>{
@@ -27,44 +31,57 @@ class User {
     return null;
   }
   static async findOneByEmailAndPassword(email: string, password: string): Promise<IUser | null>{
-    const user = await Common.dbFetch(User.tableName, { email });
-    console.log(user, password);
-    const rows = await Common.dbFetch(User.tableName, { email, password});
-    console.log(rows);
-    if(rows == undefined)
-      return null;
-    if(rows.length){
-      return rows[0] as IUser;
+    try {
+      const user = await Common.dbFetch(User.tableName, { email });
+      console.log(user, password);
+      const rows = await Common.dbFetch(User.tableName, { email, password});
+      console.log(rows);
+      if(rows == undefined)
+        return null;
+      if(rows.length){
+        return rows[0] as IUser;
+      }
+    } catch (error) {
+      Logger.log('User Model Error', error);
+    }
+
+    return null;
+  }
+
+  static async findAll(): Promise<IUserSerialized[] | null>{
+    try {
+      const rows = await Common.dbFetch(
+        User.tableName,
+        null,
+        [
+          'id',
+          'firstname',
+          'lastname',
+          'email',
+          'created_at',
+        ],
+      );
+      return rows as IUserSerialized[];
+    } catch (error) {
+      Logger.log('User Model Error', error);
     }
     return null;
   }
 
-  static async findAll(): Promise<IUserSerialized[]>{
-    const rows = await Common.dbFetch(
-      User.tableName,
-      null,
-      [
-        'id',
-        'firstname',
-        'lastname',
-        'email',
-        'created_at',
-      ],
-    );
-    return rows as IUserSerialized[];
-  }
-
   static async create(user: ICreateUser): Promise<IUserSerialized | null>{
-    const insertQuery = await Common.dbInsertion(User.tableName, user);
-    if(insertQuery && insertQuery.inserted){
-      const newUser = insertQuery.data[0] as IUserSerialized;
+    try {
+      const insertQuery = await Common.dbInsertion(User.tableName, user);
+      if(insertQuery && insertQuery.inserted){
+        const newUser = insertQuery.data[0] as IUserSerialized;
 
-      // removing password from the object before serializing it
-      newUser.password = undefined;
-      return newUser;
-    }else{
-      return null;
+        // removing password from the object before serializing it
+        newUser.password = undefined;
+        return newUser;
+      }
+    } catch (error) {
+      Logger.log('User Model Error', error);
     }
+    return null;
   }
 }
 
