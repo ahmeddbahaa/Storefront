fde71cef143ee5309f3c131253194f51e6db2476
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-02-26 04:09:35 +0200

    Added Database Operations Unit Tests

---
+++ src/utils/common.spec.ts
@@ -0,0 +1,53 @@
+import { truncateDB } from '../../spec/utils';
+import {ICreateUser} from '../components/user/user.interfaces';
+import Common from './common';
+import {addTestUser} from '../../spec/utils/index';
+
+describe('[E2E] Database Operations', function() {
+
+  describe('Create Operation', function() {
+    beforeAll(async() => {
+      await truncateDB();
+    });
+    // Success scenarios
+    it(' Creates a Test User', async function() {
+      // status code should be 201 `Created`
+      const testUser:ICreateUser = {
+        firstname: 'test',
+        lastname: 'test',
+        email:'admin@example.com',
+        password: '123456789',
+      };
+      const user = await Common.dbInsertion('users', testUser);
+      expect(user?.data[0]).toBeTruthy();
+    });
+  });
+  describe('Fetch Operation', function() {
+    beforeAll(async() => {
+      await truncateDB();
+    });
+    // Success scenarios
+    it('Creates a Test User', async function() {
+      // status code should be 201 `Created`
+      const testUser:ICreateUser = {
+        firstname: 'test',
+        lastname: 'test',
+        email:'admin@example.com',
+        password: '123456789',
+      };
+      const user = await Common.dbInsertion('users', testUser);
+      expect(user?.data[0]).toBeTruthy();
+    });
+    it('Show The Test User by Email', async function() {
+    // status code should be 201 `Created`
+      const user = await Common.dbFetch('users', {email:'admin@example.com'});
+      expect(user?.[0]).toBeTruthy();
+    });
+    it('index The Test Users', async function() {
+      // status code should be 201 `Created`
+      const user = await Common.dbFetch('users');
+      expect(user?.[0]).toBeTruthy();
+    });
+  });
+
+});
