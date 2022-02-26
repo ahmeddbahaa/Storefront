211eafd0550b9c1edd2b4f42ad462e6e6ce865eb
ahmeddbahaa <ahmedbahaa0026@gmail.com>
2022-02-26 03:42:55 +0200

    Added Try-catch blocks for asynchronous methods in order component

--- src/components/order/order.controller.ts
+++ src/components/order/order.controller.ts
@@ -6,48 +6,76 @@ import {
   ORDER_STATUS,
 } from './order.interfaces';
 import Order from './order.model';
+import Logger from '../../middlewares/logger';
 class OrderController {
   async getOrderById(req: Request, res: Response): Promise<void> {
-    const order = await Order.findOrderById(+req.params.id);
-    if (!order) {
-      res.status(400).send('Order Not Found');
-    } else {
-      res.send(order);
+    try {
+      const order = await Order.findOrderById(+req.params.id);
+      if (!order) {
+        res.status(400).send('Order Not Found');
+      } else {
+        res.send(order);
+      }
+    } catch (error) {
+      Logger.log('Order Controller Error: ', error);
     }
   }
   async getUserOrders(req: Request, res: Response): Promise<void> {
-    const order = await Order.findUserOrders(+req.params.user_id);
-    if (!order) {
-      res.status(400).send('Order Not Found');
-    } else {
-      res.send(order);
+    try {
+      const order = await Order.findUserOrders(+req.params.user_id);
+      if (!order) {
+        res.status(400).send('Order Not Found');
+      } else {
+        res.send(order);
+      }
+    } catch (error) {
+      Logger.log('Order Controller Error:', error);
     }
   }
   async getCompleteUserOrders(req: Request, res: Response): Promise<void> {
-    const order = await Order.findCompeleteOrders(+req.params.user_id);
-    if (!order) {
-      res.status(400).send('Order Not Found');
-    } else {
-      res.send(order);
+    try {
+      const order = await Order.findCompeleteOrders(+req.params.user_id);
+      if (!order) {
+        res.status(400).send('Order Not Found');
+      } else {
+        res.send(order);
+      }
+    } catch (error) {
+      Logger.log('Order Controller Error', error);
     }
   }
   async getOrders(req: Request, res: Response): Promise<void> {
-    const orders = await Order.findAll();
-    res.send(orders);
+    try {
+      const orders = await Order.findAll();
+      res.send(orders);
+    } catch (error) {
+      Logger.log('Order Controller Error', error);
+    }
+
   }
   async create(req: Request, res: Response): Promise<void> {
-    const { user_id, order_status } = req.body;
-    const dataObject: ICreateOrder = { user_id, order_status };
-    const order = await Order.create(dataObject);
-    res.status(201).send(order);
+    try {
+      const { user_id, order_status } = req.body;
+      const dataObject: ICreateOrder = { user_id, order_status };
+      const order = await Order.create(dataObject);
+      res.status(201).send(order);
+    } catch (error) {
+      Logger.log('Order Controller Error', error);
+      res.status(500).send('Error Please Try Again Later');
+    }
   }
   async addProduct(req: Request, res: Response): Promise<void> {
-    const order_id: number = +req.params.id;
-    const quantity:number = parseInt(req.body.quantity);
-    const product_id:number = parseInt(req.body.product_id);
-    const dataObject: IOrderProduct = { order_id, quantity , product_id };
-    const result = await Order.addProduct(dataObject);
-    res.status(201).send(result);
+    try {
+      const order_id: number = +req.params.id;
+      const quantity:number = parseInt(req.body.quantity);
+      const product_id:number = parseInt(req.body.product_id);
+      const dataObject: IOrderProduct = { order_id, quantity , product_id };
+      const result = await Order.addProduct(dataObject);
+      res.status(201).send(result);
+    } catch (error) {
+      Logger.log('Order Controller Error', error);
+      res.status(500).send('Error Please Try Again Later');
+    }
   }
 }
 export default new OrderController();
--- src/components/order/order.model.ts
+++ src/components/order/order.model.ts
@@ -8,18 +8,22 @@ import {
   IOrderSeralized,
   ORDER_STATUS,
 } from './order.interfaces';
+import Logger from '../../middlewares/logger';
 
 class Order {
   static tableName: string = 'orders';
 
   static async create(order: ICreateOrder): Promise<IOrder | null> {
-    const insertQuery = await Common.dbInsertion(Order.tableName, order);
-    if (insertQuery && insertQuery.inserted) {
-      const order = insertQuery.data[0] as IOrder;
-      return order;
-    } else {
-      return null;
+    try {
+      const insertQuery = await Common.dbInsertion(Order.tableName, order);
+      if (insertQuery && insertQuery.inserted) {
+        const order = insertQuery.data[0] as IOrder;
+        return order;
+      }
+    } catch (error) {
+      Logger.log('Order Model Error: ', error);
     }
+    return null;
   }
 
   static async findAll(): Promise<IOrderSeralized[] | null> {
@@ -28,48 +32,60 @@ class Order {
   }
   static async findCompeleteOrders(
     user_id: number): Promise<IOrderSeralized[] | null> {
-    const condition = { user_id: user_id, order_status: ORDER_STATUS.COMPLETE };
-    const rows = await Common.dbFetch(Order.tableName, condition);
-    if (rows?.length) {
-      const orders = rows as IOrderSeralized[];
-      return orders;
-    } else {
-      return null;
+    try {
+      const condition = {
+        user_id: user_id,
+        order_status: ORDER_STATUS.COMPLETE,
+      };
+      const rows = await Common.dbFetch(Order.tableName, condition);
+      if (rows?.length) {
+        const orders = rows as IOrderSeralized[];
+        return orders;
+      }
+    } catch (error) {
+      Logger.log('Order Model Error: ', error);
     }
+    return null;
   }
-  static async findUserOrders(
-    user_id: number): Promise<IOrderSeralized[] | null> {
-    const condition = { user_id: user_id};
-    const rows = await Common.dbFetch(Order.tableName, condition);
-    if (rows?.length) {
-      const order = rows as IOrderSeralized[];
-      return order;
-    } else {
-      return null;
+  static async findUserOrders(user_id: number): Promise<IOrderSeralized[] | null> {
+    try {
+      const condition = { user_id: user_id };
+      const rows = await Common.dbFetch(Order.tableName, condition);
+      if (rows?.length) {
+        const order = rows as IOrderSeralized[];
+        return order;
+      }
+    } catch (error) {
+      Logger.log('Order Model Error: ', error);
     }
+    return null;
   }
   static async findOrderById(id: number): Promise<IOrderSeralized | null> {
-    const condition = { id: id };
-    const rows = await Common.dbFetch(Order.tableName, condition);
-    if (rows?.length) {
-      const order = rows[0] as IOrderSeralized;
-      return order;
-    } else {
-      return null;
+    try {
+      const condition = { id: id };
+      const rows = await Common.dbFetch(Order.tableName, condition);
+      if (rows?.length) {
+        const order = rows[0] as IOrderSeralized;
+        return order;
+      }
+    } catch (error) {
+      Logger.log('Order Model Error: ', error);
+
     }
+    return null;
   }
 
-  static async addProduct(
-    order_product: IOrderProduct): Promise<IOrderProductSeralized | null> {
-    const insertedQuery = await Common.dbInsertion(
-      'order_products',
-      order_product);
-    if (insertedQuery && insertedQuery.inserted) {
-      const orderProduct = insertedQuery.data[0] as IOrderProductSeralized;
-      return orderProduct;
-    } else {
-      return null;
+  static async addProduct(order_product: IOrderProduct): Promise<IOrderProductSeralized | null> {
+    try {
+      const insertedQuery = await Common.dbInsertion('order_products', order_product);
+      if(insertedQuery && insertedQuery.inserted){
+        const orderProduct = insertedQuery.data[0] as IOrderProductSeralized;
+        return orderProduct;
+      }
+    } catch (error) {
+      Logger.log('Order Model Error: ', error);
     }
+    return null;
   }
 }
 export default Order;
